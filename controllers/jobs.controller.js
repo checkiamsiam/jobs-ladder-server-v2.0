ebc86e6d0aa1b8ae084b3dc80bfa72dfaa071353
checkiamsiam <issiam02415@gmail.com>
2022-10-13 23:30:35 +0600

    get route created

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -1,16 +1,66 @@
-const Job = require("../models/jobs.modal")
+const Job = require("../models/jobs.modal");
+const UserModel = require("../models/user.modal");
+const mongoose = require("mongoose");
+const {} = mongoose.Types;
 
-module.exports.createJob = async (req , res , next) => {
+module.exports.getAllJobs = async (req, res, next) => {
   try {
-    const job = new Job(req.body)
-    job.postedBy = {
-      name : req.user.name ,
-      id : req.user.id
-    }
-    await job.save()
-    res.json({success : true , message : "job posted successful"})
-
+    const jobs = await Job.find();
+    res.json({ success: true, data: jobs });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
+module.exports.createJob = async (req, res, next) => {
+  try {
+    const job = new Job(req.body);
+    job.postedBy = {
+      name: req.user.name,
+      id: req.user.id,
+    };
+    const thatJob = await job.save();
+    await UserModel.findByIdAndUpdate(req.user.id, { $push: { jobPosts: thatJob.id } });
+    res.json({ success: true, message: "job posted successful" });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.findByPoster = async (req, res, next) => {
+  try {
+    const jobs = await Job.find({ "postedBy.id": req.user.id });
+    res.send({ success: true, data: jobs });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.findSpecificJob = async (req, res, next) => {
+  try {
+    const job = await Job.findById(req.params.id).populate("candidates.id");
+    if (!job.postedBy.id.equals(req.user.id)) {
+      return res.status(403).json({ success: false, message: "unauthorized access" });
+    }
+    res.send({ success: true, data: job });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.getSpecificWithManager = async (req, res, next) => {
+  try {
+    const job = await Job.findById(req.params.id).populate("postedBy.id" , "-password");
+    res.send({ success: true, data: job });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.updateJob = async (req, res, next) => {
+  try {
+    const job = await Job.findById(req.params.id);
+    if (!job.postedBy.id.equals(req.user.id)) {
+      return res.status(403).json({ success: false, message: "unauthorized access" });
+    }
+    await Job.findByIdAndUpdate(req.params.id, { $set: req.body });
+    res.json({ success: true, message: "data updated" });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/jobs.route.js
+++ routes/jobs.route.js
@@ -4,9 +4,11 @@ const authorization = require('../middleware/authorization');
 const verifyUser = require('../middleware/verifyUser');
 const jobsRoute = express.Router()
 
+jobsRoute.get('/jobs', verifyUser, authorization('candidate'), jobsController.getAllJobs )
 jobsRoute.post('/jobs', verifyUser, authorization('hr'), jobsController.createJob )
-// jobsRoute.get('/manager/jobs', jobsController.createJob )
-// jobsRoute.get('/manager/jobs/:id' , userController.getMe)
-// jobsRoute.patch('/jobs/:id ' , userController.getMe)
+jobsRoute.get('/jobs/:id' ,verifyUser, authorization('hr') , jobsController.getSpecificWithManager)
+jobsRoute.patch('/jobs/:id' ,verifyUser, authorization('hr') , jobsController.updateJob)
+jobsRoute.get('/manager/jobs', verifyUser, authorization('hr'), jobsController.findByPoster )
+jobsRoute.get('/manager/jobs/:id' , verifyUser, authorization('hr') , jobsController.findSpecificJob)
 
 module.exports = jobsRoute
